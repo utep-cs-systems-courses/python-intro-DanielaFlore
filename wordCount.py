ec404bf0e58993ca1df936f87956c8b28b3dd1e3
DanielaFlore <47197880+DanielaFlore@users.noreply.github.com>
2020-09-02 23:19:40 -0600

    cleaned program/added comments

--- wordCount.py
+++ wordCount.py
@@ -4,19 +4,21 @@ Created on Sun Aug 30 21:03:21 2020
 
 @author: Flores Daniela
 """
-'''
-TODO: get input file through intrctv mode
-'''
 
+'''
+#gathers arguments
+import sys
+inputFile = sys.argv[1] 
+output = sys.argv[2]
+
+f = open(inputFile, "r") 
+'''
 f = open(r"C:\...\speech.txt", "r")
 #stores set of words in speech.txt
 dictionary = []
 #stores number of times dictionary words are used
 counter =[]
 bareText = []
-#variables I used to make sure my program isnt skipping any words on file
-count = 0
-counterTwo = 0
 sortedDict = []
 #iterate through file
 for line in f:
@@ -30,28 +32,27 @@ for line in f:
     line = line.replace("!","")
     #goes word for word in file
     for word in line.split():
+        #store text with no punctuation in list
         bareText.append(word)
-        #keeps track on total amount of words
-        count = count + 1
         if word in dictionary:
             i = dictionary.index(word)
-            #counter[i] +=1
             continue
+        
         elif word not in dictionary:
-            counterTwo = counterTwo + 1
             dictionary.append(word)
-            counter.append(1)
-sortedDict = sorted(dictionary)
+            counter.append(0)
 
+#sorts dictionary alphabetically
+sortedDict = sorted(dictionary)
+#counts number of times words appear
 for word in bareText:
     i = sortedDict.index(word)
-    counter[i] +=1
+    counter[i] = counter[i] +1;
 
-for i in range(len(dictionary)):
-    print(dictionary[i] , counter[i])
+for i in range(len(sortedDict)):
+    print(sortedDict[i] , counter[i])
 
-
-    
+#creates output file and writes solution    
 outF = open("output.txt", "w")
 for i in range(len(sortedDict)):
   # write line to output file
@@ -62,5 +63,6 @@ for i in range(len(sortedDict)):
 outF.close()
     
     
+
 
 
